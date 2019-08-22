1ab93fc19404dae3d41bcfb82dfdd037fb43fca6
yuyufenf <kampf950510@hotmail.com>
2019-08-22 11:39:12 +0800

    19年8月22日 11:38 修改启动项，修改pom

--- src/main/java/com/gundam/unicorn/UnicornApplication.java
+++ src/main/java/com/gundam/unicorn/UnicornApplication.java
@@ -1,13 +1,24 @@
 package com.gundam.unicorn;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
-public class UnicornApplication {
+/**
+ * 启动项
+ * @author kampf
+ * @date 2019/08/22
+ */
+@Configuration
+@EnableAutoConfiguration
+public class UnicornApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(UnicornApplication.class, args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+       return application.sources(UnicornApplication.class);
     }
 
 }
