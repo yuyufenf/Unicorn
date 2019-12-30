c7b8fa0a60096d790dbd74acc1e531cef11755de
yuyufenf <kampf950510@hotmail.com>
2019-12-30 17:02:38 +0800

    2019/12/30 17:00 修改文件存储组件,添加遗漏的redis工具

--- src/main/java/com/gundam/unicorn/utils/FilePathUtils.java
+++ src/main/java/com/gundam/unicorn/utils/FilePathUtils.java
@@ -1,57 +1,165 @@
 package com.gundam.unicorn.utils;
 
+import com.gundam.unicorn.utils.exception.FileException;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.BufferedWriter;
 import java.io.File;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
+ * 文件储存工具类
  * @author kampf
  * @date 2019/8/24 11:20
  */
+@Slf4j
 public class FilePathUtils {
 
+    //TODO 所有的路径要做成 自定义配置文件存储地址 + 项目名称 + 日期文件夹 + 文件
+
+    /** 自定义配置文件存储地址 **/
+    private static String staticDir;
+
+    /** 项目名称 **/
+    private static String fileDir = "unicorn";
+
     /**
-     * 将文件名添加4位纯数字uuid以防止文件名重复
-     * @param fileName
+     * 储存文件工具类(字节文件)
+     * @param fileName 文件名
+     * @param fileType 文件类型,如果类型不是自定义请写空
+     * @param file 二进制文件
+     * @return 文件储存
+     */
+    public static Map<String, String> saveFile(String fileName, String fileType, byte file){
+        try {
+
+            Map<String, String> resultPath = new HashMap<>();
+
+            if(!StringUtils.isBlank(fileType)){
+                fileName = fileName + "." + fileType;
+            }
+
+            fileName = makeFileName(fileName);
+
+            Map<String, String> paths = makePath();
+
+            //相对路径
+            String relPath = paths.get("relPath") + fileName;
+            //绝对路径
+            String absPath = paths.get("absPath") + fileName;
+
+            BufferedWriter writer = Files.newBufferedWriter(Paths.get(absPath));
+            writer.write(file);
+            writer.flush();
+            writer.close();
+
+            resultPath.put("relPath", relPath);
+            resultPath.put("absPath", absPath);
+
+            return resultPath;
+        } catch (Exception e){
+            log.error("文件存储失败:[{}]", e.getMessage(), e);
+            throw new FileException("创建文件时失败,请检查存储路径是否存在权限问题");
+        }
+    }
+
+    /**
+     * 储存文件工具类(流文件)
+     * @param fileName 文件名
+     * @param fileType 文件类型
+     * @param is 流文件
      * @return
      */
-    public static String makeFileName(String fileName){
+    public static Map<String, String> saveFile(String fileName, String fileType, InputStream is){
+        try {
+
+            Map<String, String> resultPath = new HashMap<>();
+
+            if(StringUtils.isBlank(fileType)){
+                fileName = fileName + "." + fileType;
+            }
+
+            fileName = makeFileName(fileName);
+
+            Map<String, String> paths = makePath();
+
+            //相对路径
+            String relPath = paths.get("relPath") + fileName;
+            //绝对路径
+            String absPath = paths.get("absPath") + fileName;
+
+            Files.createFile(Paths.get(absPath));
+
+            resultPath.put("relPath", relPath);
+            resultPath.put("absPath", absPath);
+
+            return resultPath;
+        } catch (Exception e){
+            log.error("文件存储失败:[{}]", e.getMessage(), e);
+            throw new FileException("创建文件时失败,请检查存储路径是否存在权限问题");
+        }
+    }
+
+    /**
+     * 将文件名添加4位纯数字uuid以及日期以防止文件名重复
+     * @param fileName 原始文件名
+     * @return
+     */
+    private static String makeFileName(String fileName){
+
+        Date date = new Date();
+        SimpleDateFormat dateFormat= new SimpleDateFormat("HHmm");
+
         int hashCode = UUID.randomUUID().hashCode();
         if(hashCode < 0){
             hashCode = -hashCode;
         }
 
         String uuid = String.format("%04d", hashCode);
-        fileName = uuid + fileName;
+        fileName = dateFormat.format(date) + uuid + fileName;
 
         return fileName;
     }
 
     /**
-     * 制作文件储存路径，返回相对路径和绝对路径
-     * @return
+     * 通过规则创建文件储存路径
+     * @return 返回绝对路径(absPath)和相对路径(relPath)的map对象
      */
-    public static Map<String, String> makePath(String filePath){
+    private static Map<String, String> makePath(){
 
-        Map<String, String> map = new HashMap<String, String>();
+        Map<String, String> map = new HashMap<>();
 
-        Calendar date = Calendar.getInstance();
-        //根据年月日创建目录并分割
-        String path = date.get(Calendar.YEAR) + File.separator
-                + (date.get(Calendar.MONTH) + 1) + File.separator
-                + date.get(Calendar.DAY_OF_MONTH);
-        //将所有的双右斜杠替换成单左斜杠
-        path = path.replaceAll("\\\\", "/");
-        File up = new File(filePath + "/" + path);
-        //判断目录是否存在
-        if(!up.exists()){
-            up.mkdirs();
+        Date date = new Date();
+        SimpleDateFormat dateFormat= new SimpleDateFormat("yyyyMM");
+
+        String relPath = "/" + fileDir + "/" + dateFormat.format(date) + "/";
+        String absPath = staticDir + relPath;
+
+        //TODO 根据年月日创建目录并分割,不过一般用不到这么细分,常规储存一般细分到月即可.之前有做过监控项目,图片的归类细分到秒,所以文件夹都是细分到天故保留此分割方法
+//        Calendar date = Calendar.getInstance();
+//        根据年月日创建目录并分割
+//        String path = date.get(Calendar.YEAR) + File.separator
+//                + (date.get(Calendar.MONTH) + 1) + File.separator
+//                + date.get(Calendar.DAY_OF_MONTH);
+        Path up = Paths.get(absPath);
+
+        try {
+            if(!Files.exists(up)){
+                Files.createDirectories(up);
+            }
+        } catch (Exception e){
+            log.error("路径创建失败:[{}]", e.getMessage(), e);
+            throw new FileException("创建文件夹路径时失败,请检查存储路径是否存在权限问题");
         }
 
-        map.put("relPath",path);
-        map.put("absPath",filePath + "/" + path);
+        map.put("relPath",relPath);
+        map.put("absPath",absPath);
 
         return map;
     }
